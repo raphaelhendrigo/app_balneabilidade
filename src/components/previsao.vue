34ddf741b890461393d8f73badac27a6d84afcf8
ivancr2 <ivancalixto1@gmail.com>
2020-10-14 10:47:39 -0300

    primeiro esboço tab-panels c/ gráfico

---
+++ src/components/previsao.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="q-pa-md">
+    <p>
+      Previsão referente somente à praia "Grande" da cidade de "Ubatuba"
+    </p>
+    <q-table
+      title="Previsão Próximas 5 semanas"
+      :data="previsaoProximasCincoSemanas"
+      :columns="colunasTabelaPrevisao"
+      row-key="id"
+    >
+    </q-table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      filter: "",
+      loading: false,
+      colunasTabelaPrevisao: [
+        {
+          name: "dataMedicao",
+          required: true,
+          label: "Data medição",
+          align: "left",
+          field: row => row.dataMedicao,
+          format: val => `${val}`,
+          sortable: false
+        },
+        {
+          name: "enterococos",
+          align: "center",
+          label: "UFC/100 ml",
+          field: "enterococos",
+          sortable: false
+        }
+      ],
+      mensagem: "",
+      previsaoProximasCincoSemanas: [],
+      cardMensagem: ""
+    };
+  },
+  computed: {
+    cidade: function() {
+      return this.$store.getters["exemplo/getCidade"];
+    },
+    praia: function() {
+      return this.$store.getters["exemplo/getPraia"];
+    },
+    ip_webservice: function() {
+      return this.$store.getters["exemplo/getIpWebservice"];
+    }
+  },
+  async mounted() {
+    if (this.cidade != "" && this.praia != "") {
+      await this.retornaPrevisaoProximasCincoSemanas();
+    }
+  },
+  methods: {
+    async retornaPrevisaoProximasCincoSemanas() {
+      await axios({
+        method: "GET",
+        url:
+          "http://" +
+          this.ip_webservice.concat(":5000/previsaoProximasCincoSemanas")
+      }).then(
+        result => {
+          this.previsaoProximasCincoSemanas = result.data.map((item, key) => {
+            let arr = [];
+            arr["dataMedicao"] = item[0];
+            arr["enterococos"] = parseFloat(item[1]).toFixed(2);
+            //arr["enterococos"] = item[1];
+            arr["id"] = key;
+            return arr;
+          });
+        },
+        error => {
+          console.error(error);
+        }
+      );
+    }
+  }
+};
+</script>
